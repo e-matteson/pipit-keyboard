fbc6a603a613bce8cdd618a4332bf906a2ce0db3
em <e.r.matteson@gmail.com>
2017-03-13 11:09:38 -0400

    add OpDefBuilder

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,132 @@
 extern crate toml;
-
-use std::collections::HashMap;
 use toml::Value;
 
-enum OptionType{
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Debug)]
+enum OpType{
     Mode {useWords: bool},
     DefineInt,
     DefineString,
     IfdefValue,
     IfdefKey,
     Array1D,
-    Array2D {dim2: u32},
+    Array2D,
     Internal,
 }
 
-enum OptionVal{
-    Str (Option<String>),
-    Int (Option<i64>),
+#[derive(Debug)]
+enum OpVal{
+    Str (String),
+    Int (i64),
+    Bool (bool),
     // Array {val: },
 }
 
-
-enum OptionReq{
+#[derive(Debug)]
+enum OpReq{
     Optional,
     Required,
-    Dependent{key:String, val:String}
+    Dependent{key:String, val:Option<OpVal>}
 }
 
-struct OptionDef{
-    optype: OptionType,
+#[derive(Debug)]
+struct OpDef{
+    opType: OpType,
+    val: Option<OpVal>,
+    default: Option<OpVal>,
+    required: OpReq,
     auto: bool,
-    required: OptionReq,
-    // default:
 }
 
-fn makeOpDefMap() -> HashMap<String, OptionDef> {
-    let mut opDefMap: HashMap<String, OptionDef> = HashMap::new();
-    opDefMap.insert("num_rows".to_string(),
-                    OptionDef{auto: true,
-                              required: OptionReq::Required,
-                              optype: OptionType::DefineInt});
-    opDefMap.insert("num_cols".to_string(),
-                    OptionDef{auto: true,
-                              required: OptionReq::Required,
-                              optype: OptionType::DefineInt});
-    opDefMap.insert("false_ifdef".to_string(),
-                    OptionDef{auto: true,
-                              required: OptionReq::Required,
-                              optype: OptionType::IfdefKey});
-    opDefMap.insert("mode_name".to_string(),
-                    OptionDef{auto: true,
-                              required: OptionReq::Required,
-                              optype: OptionType::Mode{useWords: true}});
+struct OpDefBuilder{
+    opType: OpType,
+    val: Option<OpVal>,
+    default: Option<OpVal>,
+    required: OpReq,
+    auto: bool,
+}
+
+impl OpDefBuilder{
+    fn new(opType: OpType) -> OpDefBuilder{
+        OpDefBuilder{opType: opType,
+                     val: None,
+                     default: None,
+                     required: OpReq::Required,
+                     auto: false,}
+    }
+
+    fn val(mut self, val: OpVal) -> OpDefBuilder{
+        self.val = Some(val);
+        self
+    }
+    fn default(mut self, default: OpVal) -> OpDefBuilder{
+        self.default = Some(default);
+        self
+    }
+    fn required(mut self, required: OpReq) -> OpDefBuilder{
+        self.required = required;
+        self
+    }
+    fn auto(mut self, auto: bool) -> OpDefBuilder{
+        self.auto = auto;
+        self
+    }
+    fn finalize(self) -> OpDef{
+        OpDef{opType: self.opType,
+              val: self.val,
+              default: self.default,
+              required: self.required,
+              auto: self.auto,}
+    }
+
+}
+
+fn defineOptions() -> HashMap<String, OpDef> {
+    /// Define all options here.
+    let mut opDefMap: HashMap<String, OpDef> = HashMap::new();
+    opDefMap.insert(
+        "num_rows".to_string(),
+        OpDefBuilder::new(OpType::DefineInt)
+            .auto(true)
+            .finalize());
+    opDefMap.insert(
+        "num_cols".to_string(),
+        OpDefBuilder::new(OpType::DefineInt)
+            .default(OpVal::Int(3))
+            .auto(false)
+            .finalize());
+    opDefMap.insert(
+        "false_ifdef".to_string(),
+        OpDefBuilder::new(OpType::IfdefKey)
+            .default(OpVal::Bool(false))
+            .required(OpReq::Optional)
+            .finalize());
+    opDefMap.insert(
+        "mode_name".to_string(),
+        OpDefBuilder::new(OpType::Mode{useWords: false})
+            .required(OpReq::Required)
+            .finalize());
     opDefMap
 }
 
-fn makeOpValMap() -> HashMap<String, OptionVal>{
-    let mut opValMap: HashMap<String, OptionVal> = HashMap::new();
-    opValMap.insert("num_rows".to_string(), OptionVal::Int(Some(42)));
-    opValMap.insert("num_cols".to_string(), OptionVal::Int(None));
-    opValMap.insert("mode_name".to_string(), OptionVal::Str(Some("normal".to_string())));
-    opValMap.insert("false_ifdef".to_string(), OptionVal::Str(None));
-    opValMap
-}
-
-fn main() {
-    let value = "foo = 'bar'".parse::<Value>().unwrap();
-    println!(value);
-
-    // let op = OptionVal::Int(42);
-    let opDefMap = makeOpDefMap();
-    let opValMap = makeOpValMap();
-
-    for (name, op) in opValMap.iter(){
-        let def: &OptionDef = opDefMap.get(name).expect("Unknown type!");
-
+fn setOptions(map: &mut HashMap<String, OpDef>) {
+    // TODO implement
+    // also needs to take the data containing option values...
+    for (name, op) in map.iter(){
         println!("{}", name);
-        match op{
-            &OptionVal::Str(Some(ref val)) => println!("str val: {}", val),
-            &OptionVal::Str(None) => println!("str NONE"),
-            &OptionVal::Int(ref foo) => {
-                if foo.is_some(){
-                    println!("{}", foo.unwrap());
-                }
-                else {
-                    println!("int NONE");
-                }
-            },
-            // _ => println!("failed")
-        }
+        println!("{:?}", op);
     }
 }
 
-// fn old() {
-//     let mut opt: OptionType = OptionType::IfdefValue{val: "debug_messages".to_string()};
-//     let mut opt2: OptionType = OptionType::DefineInt{val: 3};
-//     let mut opt3: OptionType = OptionType::Mode{useWords: false, val: "normal".to_string()};
-//     opt3.val = "insert";
-//     match opt3{
-//         OptionType::DefineInt{val: val} => println!("{}", val),
-//         OptionType::IfdefValue{val: val} => println!("{}", val),
-//         OptionType::Mode{val: val, ..} => println!("{}", val),
-//         _ => println!("unknown"),
-//     }
-
-//     // let mut map: HashMap<&str, i32> = HashMap::new();
-//     // map.insert("a", 1);
-//     // map.insert("b", 2);
-//     // for key in map.keys(){
-//     //     println!("{}", key);
-//     // }
-//     // let val: i32 = map["a"];
-//     // println!("{}", val );
-// }
+fn main() {
+    // let value = "foo = 'bar'".parse::<Value>().unwrap();
+    // println!(value);
+    let value: OpVal = OpVal::Int(42);
+    let t: OpType = OpType::Mode{useWords: true };
+    println!("{:?}", value);
+    println!("{:?}", t);
+    let opDefMap = defineOptions();
+    println!("{:#?}", opDefMap);
+}
