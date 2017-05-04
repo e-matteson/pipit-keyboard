0d2abad04a6b1c5d5d6bdbafa01eb85593d4c647
em <e.r.matteson@gmail.com>
2017-05-04 19:06:06 -0400

    take settings file as command line option

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,24 @@
 extern crate pipit_config;
 
 use pipit_config::Maps;
+use std::env::args;
+
+const DEFAULT_SETTINGS_FILE: &str = "settings/settings.toml";
+
+fn get_settings_path() -> String {
+    // check command line arguments
+    match args().len() {
+        0 => panic!("Zero arguments!?"),
+        1 => DEFAULT_SETTINGS_FILE.to_string(),
+        2 => args().nth(1).unwrap(),
+        _ => panic!("Too many arguments"), // TODO print usage
+    }
+}
 
 
 fn main() {
-
-    let maps = Maps::load("settings/settings.toml");
+    let settings_path = get_settings_path();
+    let maps = Maps::load(&settings_path);
 
     // TODO automatically extract path parts
     let f = maps.format("auto_config");
